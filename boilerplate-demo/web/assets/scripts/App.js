ef304e571dca2b5b728a60a575aff99166f9337e
Jonathan Eddy <jonathanceddy@gmail.com>
2015-11-29 17:02:38 -0500

    put friendly feud and boilerplate demo in subdirectories

---
+++ boilerplate-demo/web/assets/scripts/App.js
@@ -0,0 +1,14 @@
+define(['exports', 'module'], function (exports, module) {
+    'use strict';
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+    var App = function App() {
+        _classCallCheck(this, App);
+
+        console.log('Your JavaScript is hooked up!');
+    };
+
+    module.exports = App;
+});
+//# sourceMappingURL=App.js.map
