3059272b6dee8500374933ed3b574e05a4870f1a
Jonathan Eddy <jonathanceddy@gmail.com>
2016-02-23 17:27:34 -0500

    fixed keyboard events and restricting when they occur

--- slipnslider/assets/scripts/components/SlipnSlider.js
+++ slipnslider/assets/scripts/components/SlipnSlider.js
@@ -690,12 +690,15 @@ define(['exports', 'module'], function (exports, module) {
     }, {
       key: 'onKeyDown',
       value: function onKeyDown(e) {
+        if (e.srcElement.localName !== 'body') {
+          return this;
+        }
         // might want to have a debounce to limit calls but behaves
         // as anticipated and isnt too overloading
         if (event.keyCode === 37) {
-          this.moveToAdjacentSlide(false);
+          this.determineAction(false);
         } else if (e.keyCode === 39) {
-          this.moveToAdjacentSlide(true);
+          this.determineAction(true);
         }
 
         return this;
