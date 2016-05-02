9105da5a01fad379d2649b069fffeea039856996
Jonathan Eddy <jonathanceddy@gmail.com>
2016-05-02 12:43:48 -0400

    added alert

--- slipnslider/assets/scripts/components/SlipnSlider.js
+++ slipnslider/assets/scripts/components/SlipnSlider.js
@@ -829,7 +829,7 @@ define(['exports', 'module'], function (exports, module) {
        */
     }, {
       key: 'prevent',
-      value: function(e) { e.preventDefault(); }
+      value: function(e) { e.preventDefault(); alert('prevent') }
     }, {
       key: 'onDrag',
       value: function onDrag(e) {
